b0417641cc656231269b047721d2ec3c8ef9a04e
chinky23-tech <chinky0330@gmail.com>
2025-07-31 23:44:50 +0530

    structure

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const searchBar = document.querySelector('.search-bar');
+
+//hamburger mobile menu logic
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
 hamburger.addEventListener('click', () => {
-navLinks.classList.toggle('active');
-searchBar.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('show');
+});
+
+//theme toggle logic
+
+const toggleButtons = document.querySelectorAll('.theme-toggle');
+
+toggleButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    const icon = button.querySelector('i');
+    if (icon.classList.contains('fa-moon')) {
+      icon.classList.remove('fa-moon');
+      icon.classList.add('fa-sun');
+    } else {
+      icon.classList.remove('fa-sun');
+      icon.classList.add('fa-moon');
+    }
+  });
 });
